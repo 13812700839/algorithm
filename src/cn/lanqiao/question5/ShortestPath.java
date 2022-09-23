16e40f1e01dc50befb41b5ab4cd4fe8b60c0313a
Pengcc <pcc_pengcc@163.com>
2022-09-23 17:16:53 +0800

    F 修改类注释格式

--- src/cn/lanqiao/question5/ShortestPath.java
+++ src/cn/lanqiao/question5/ShortestPath.java
@@ -3,12 +3,13 @@ package cn.lanqiao.question5;
 import java.util.Scanner;
 
 /**
- * 最短路
- * 题目描述：如图（图见文件img.png）所示，G 是一个无向图，其中蓝色边的长度是 1、橘色边的长度是 2、绿色边的长度是 3。
- *          则从 A 到 S 的最短距离是多少？
- * 难度：简单
- * 标签：填空题, 2019, 省赛
- * 时空限制：1s/128M
+ * <p>最短路</p>
+ * <p>题目描述：如图所示，G 是一个无向图，其中蓝色边的长度是 1、橘色边的长度是 2、绿色边的长度是 3。则从 A 到 S 的最短距离是多少？</p>
+ * <img src="img.png" />
+ * <p>难度：简单</p>
+ * <p>标签：填空题, 2019, 省赛</p>
+ * <p>时空限制：1s/128M</p>
+ * <p><a href="https://www.lanqiao.cn/problems/609/learning/">题目链接地址</a></p>
  *
  * @author Pengcc
  * @version 1.0 - 2022/09/21
